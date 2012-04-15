525fe7fdadcfc34d5969c2a2673bcbd4da1173ce
Francois Bernier <frankbernier@gmail.com>
2012-04-15 12:19:20 -0400

    Added test for issue #1395

--- core/spec/lib/search/base_spec.rb
+++ core/spec/lib/search/base_spec.rb
@@ -14,6 +14,18 @@ describe Spree::Core::Search::Base do
     searcher.retrieve_products.count.should eq 2
   end
 
+  it "switches to next page according to the page parameter" do
+    @product3 = Factory(:product, :name => "RoR Pants", :price => 14.00, :on_hand => 1)
+
+    params = { :per_page => "2" }
+    searcher = Spree::Core::Search::Base.new(params)
+    searcher.retrieve_products.count.should eq 2
+
+    params.merge! :page => "2"
+    searcher = Spree::Core::Search::Base.new(params)
+    searcher.retrieve_products.count.should eq 1
+  end
+
   it "maps search params to named scopes" do
     params = { :per_page => "",
                :search => { "price_range_any" => ["Under $10.00"] }}
